7bc9d6d8418f1e4e3458b6edb6c924e8a142d0f4
Håkon Gimse <hakon.gimse@ibm.com>
2021-04-03 13:17:12 +0200

    avatrar package

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import TopList from './TopList'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Influence Norge</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <link rel="icon" type="image/png" href="logo.svg" sizes="16x16" />
+      </Helmet>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TopList/>
+
       </header>
     </div>
   );
