a2bb884c32cf98301ab29d896acda32abe641e0d
Håkon Gimse <hakon.gimse@ibm.com>
2021-04-04 14:26:27 +0200

    noe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
 import TopList from './TopList'
+import AlignItemsList from './AlignItemsList'
 import { Helmet } from 'react-helmet'
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    appBar: {
+      position: 'relative',
+    },
+    layout: {
+      width: 'auto',
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+        width: 600,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+      padding: theme.spacing(2),
+      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+        marginTop: theme.spacing(6),
+        marginBottom: theme.spacing(6),
+        padding: theme.spacing(3),
+      },
+    },
+    stepper: {
+      padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    button: {
+      marginTop: theme.spacing(3),
+      marginLeft: theme.spacing(1),
+    },
+  }));
+  const classes = useStyles();
+
+
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("/test")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <div className="App">
+    <React.Fragment>
+    <CssBaseline/>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Influence Norge</title>
         <link rel="canonical" href="http://mysite.com/example" />
         <link rel="icon" type="image/png" href="logo.svg" sizes="16x16" />
       </Helmet>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TopList/>
-
-      </header>
-    </div>
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Norges største Influences
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <TopList/>
+        </Paper>
+        
+      </main>
+      
+      </React.Fragment>
   );
 }
 
--- src/TopList.js
+++ src/TopList.js
@@ -11,7 +11,7 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '36ch',
+    maxWidth: '72ch',
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
@@ -22,75 +22,48 @@ const useStyles = makeStyles((theme) => ({
 export default function TopList() {
   const classes = useStyles();
 
+  const [topListData, setTopListData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("/getTopList")
+      .then((res) => res.json())
+      .then((data) => setTopListData(data));
+  }, []);
+  console.log(topListData)
+  if (!topListData){
+    return (<p>Loading...</p>)
+  }
   
-
   return (
     <List className={classes.root}>
+    {topListData.map(person => (
+      <React.Fragment>
       <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
+      <ListItemAvatar>
+        <Avatar alt={person.username} src="/static/images/avatar/1.jpg" />
+      </ListItemAvatar>
+      <ListItemText
+        primary={person.fullname}
+        secondary={
+          <React.Fragment>
+            <Typography
+              component="span"
+              variant="body2"
+              className={classes.inline}
+              color="textPrimary"
+            >
+              {person.username}
+            </Typography>
+            {" — Antall følgere: "}{person.instagram_sub_count}
+          </React.Fragment>
+        }
+      />
+      
+    </ListItem>
+    <Divider variant="inset" component="li" />
+    </React.Fragment>
+    
+    ))}
     </List>
+    
   );
 }
